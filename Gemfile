bbf89fe0c5c106af790edc74b28bbedfa002f35c
Franz Torghele <f.torghele@gmail.com>
2012-01-07 12:26:29 +0100

    performance testing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
 gem 'sqlite3'
+
+gem 'rack-perftools_profiler', :require => 'rack/perftools_profiler'
--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,10 +3,10 @@ require 'rails/performance_test_help'
 
 class BrowsingTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
-  # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
-  #                          :output => 'tmp/performance', :formats => [:flat] }
+  self.profile_options = { :runs => 10, :metrics => [:wall_time, :memory, :allocations, :cpu_time, :process_time, :gc_runs, :gc_time],
+                           :output => 'tmp/performance', :formats => [:flat] }
 
   def test_homepage
-    get '/'
+    get '/articles', :format => :json
   end
 end
